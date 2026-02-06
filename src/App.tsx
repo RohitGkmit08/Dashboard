4836e4a1436ba383e7dd40948497360aea5ad4a5
Rohit <sinharohit96690@gmail.com>
2026-02-06 17:59:20 +0530

    setup react router with login and dashboard routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import LoginForm from "./features/auth/login/LoginForm.jsx";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import Login from "./features/auth/login/Login";
+import Dashboard from "./features/dashboard/Dashboard";
+
 const App = () => {
   return (
-    <>
-      <LoginForm />
-    </>
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
   );
 };
 
