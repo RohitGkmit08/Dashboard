2b26e53e44af56962e52e919a3ff80dd1a72f56d
Rohit <sinharohit96690@gmail.com>
2026-02-09 14:11:29 +0530

    add dashboard mui skeleton layout

---
+++ src/features/dashboard/components/DashboardSidebar.jsx
@@ -0,0 +1,27 @@
+import { Drawer, Box, Typography } from "@mui/material";
+
+const drawerWidth = 260;
+
+const DashboardSidebar = () => {
+  return (
+    <Drawer
+      variant="permanent"
+      sx={{
+        width: drawerWidth,
+        flexShrink: 0,
+        [`& .MuiDrawer-paper`]: {
+          width: drawerWidth,
+          boxSizing: "border-box",
+        },
+      }}
+    >
+      <Box sx={{ p: 2 }}>
+        <Typography variant="h6" fontWeight="700">
+          Admin Dashboard
+        </Typography>
+      </Box>
+    </Drawer>
+  );
+};
+
+export default DashboardSidebar;
